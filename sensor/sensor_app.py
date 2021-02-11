875de362c6db059f4f0541231a36ee7db2b555e3
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-10 22:42:21 -0600

    Module #2 fix

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from datetime import date, datetime
 
 ##############################
 # Do not remove these two lines
@@ -18,7 +19,9 @@ house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
-
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_date("id", rec_date=test_date)
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
 
