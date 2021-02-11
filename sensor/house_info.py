d7e97a84c9290a90dd8fcfd4e27994b581d13ab3
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-10 22:32:51 -0600

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -17,7 +17,7 @@ class HouseInfo:
     def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
         for record in self.data:
-            if record['date'] == strftime("%m/%d/%y", rec_date):
+            if rec.date.strftime("%/m/%d/%y") == record['date']:
                 field_data.append(record[field])
         return field_data
 
