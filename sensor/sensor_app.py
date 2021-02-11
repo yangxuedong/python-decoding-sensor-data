1f92c2414dcbb57b9a1f6e8c5767b0dfd6242478
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-10 22:29:46 -0600

    Module #2

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,6 @@
 # Runner script for all modules
 from load_data import load_sensor_data
+from house_info import HouseInfo
 
 ##############################
 # Do not remove these two lines
@@ -13,6 +14,11 @@ data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 
 # Module 2 code here:
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
 
 # Module 3 code here:
 
