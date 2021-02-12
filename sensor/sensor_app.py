3492b0954309583eb5337a3f2e1e24887074371e
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-11 23:48:55 -0600

    Module #4

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,9 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date, datetime
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
+from particle_count_info import ParticleData
 
 
 ##############################
@@ -38,5 +41,33 @@ print("\nHouse Temperature sensor records for date: {} = {}".format(
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
+
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Humidity sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)
+))
+print("\tAverage: {} humidity".format(mean(recs)))
+
+particle_data = ParticleData(data)
+recs = particle_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+recs = particle_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Particle sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)
+))
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
 
 # Module 5 code here:
